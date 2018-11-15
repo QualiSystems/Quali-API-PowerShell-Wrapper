ea22f961045e64cf545d0acfcba315e37a7d6b4b
Yaniv Kalsky <yanivka@gmail.com>
2018-11-15 12:07:34 -0800

    changed message

--- QualiAPI/Test-TokenExsists.ps1
+++ QualiAPI/Test-TokenExsists.ps1
@@ -3,7 +3,7 @@
     if ($Auth -eq $null)
     {
         Write-host 'ERROR:'  -ForeGround Red
-        Write-host 'Please logon first using the Invoke-Login method'  -ForeGround Red
+        Write-host 'Please log in first using the Invoke-Login method'  -ForeGround Red
         return $false
     }
     return $true
